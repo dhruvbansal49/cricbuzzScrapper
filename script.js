2bbd9f757e8e69a42f47e29a10bdb9734d3c9d3c
bansaldhruv49 <bansaldhruv49@gmail.com>
2021-03-18 14:28:18 +0530

    Bowlers Data Fetched

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ require("chromedriver");
 let wd = require("selenium-webdriver");
 let matchId = 30880;
 let innings = 2;
-let batsmanKeys = ['Player Name','Out','Runs Scored','Balls Faced','4s','6s','Strike Rate'];
+let batsmanKeys = ['playerName','Out','runsScored','ballsFaced','foursHitted','sixesHitted','strikeRate'];
+let bowlerKeys = ['playerName','totalOver','maidenOver','runs','wicket','noBall','wide','economy'];
 let allBatsmanData=[];
+let allBowlersData=[];
 async function temp(){
     let browser = await new wd.Builder().forBrowser('chrome').build();
     await browser.get(`https://www.cricbuzz.com/live-cricket-scores/${matchId}`);
@@ -13,6 +15,9 @@ async function temp(){
     
     await browser.wait(wd.until.elementsLocated(wd.By.css(`#innings_${innings} .cb-col.cb-col-100.cb-ltst-wgt-hdr`)));
     let tables = await browser.findElements(wd.By.css(`#innings_${innings} .cb-col.cb-col-100.cb-ltst-wgt-hdr`));
+    
+    console.log(`<----- Innings ${innings} Batsmen ----->`);
+    //Fetching Batsmen Data
     let batsmanRows = await tables[0].findElements(wd.By.css(".cb-col.cb-col-100.cb-scrd-itms"));
     for(let i in batsmanRows){
         let columns = await batsmanRows[i].findElements(wd.By.css("div"));
@@ -30,6 +35,21 @@ async function temp(){
         allBatsmanData.push(singleBatsmanData);
     }
     console.log(allBatsmanData);
+
+    console.log(`<----- Innings ${innings} Bowler ----->`);
+    //Fetching Bowlers Data
+    let bowlerRows = await tables[1].findElements(wd.By.css(".cb-col.cb-col-100.cb-scrd-itms"));
+    for(let i in bowlerRows){
+        let columns = await bowlerRows[i].findElements(wd.By.css("div"));
+        let singleBowlerData={};
+        for(let j in columns){
+            let key = bowlerKeys[j];
+            let value = await columns[j].getAttribute("innerText");
+            singleBowlerData[key]=value;
+        }
+        allBowlersData.push(singleBowlerData);
+    }
+    console.log(allBowlersData);
     browser.close();
 }
 temp();
